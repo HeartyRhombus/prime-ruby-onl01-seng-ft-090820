594fa6962d09b554b0ab00f9e96378d937404dff
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-11 02:21:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    (2..number-1).each do |int|
+    if number % int == 0
+      return false
+    end
+    end
+  true
+  end
+end
